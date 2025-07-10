8a456f27abd1fa268a305e868beb11277057fd0c
João Vitor <jviitor2628@gmail.com>
2025-07-10 16:18:35 +0000

    quero mudar a logotipo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,11 +12,13 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2" />
-      <path d="M15 18H9" />
-      <path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14" />
-      <circle cx="17" cy="18" r="2" />
-      <circle cx="7" cy="18" r="2" />
+      <path d="M10 18h4" />
+      <path d="M12 18v-3.33c0-.47.24-.9.64-1.15l1.63-.94c.2-.12.44-.12.64 0l1.63.94c.4.25.64.68.64 1.15V18" />
+      <path d="M8 18V9.5c0-1.1.9-2 2-2h4c1.1 0 2 .9 2 2V18" />
+      <path d="M8 18h-2c-1.1 0-2-.9-2-2v-3.5c0-1.1.9-2 2-2h1.5" />
+      <path d="m22 7-9 9L9 12" />
+      <circle cx="8" cy="18" r="2" />
+      <circle cx="16" cy="18" r="2" />
     </svg>
   );
 }
